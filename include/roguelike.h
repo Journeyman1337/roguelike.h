4c3a7644631b426e7c6e090439d0e3dc36bb366b
Daniel Valcour <60055347+Journeyman-dev@users.noreply.github.com>
2022-07-31 22:49:52 -0400

    number steps instead of using bullets
    makes it more clear they are steps

--- include/roguelike.h
+++ include/roguelike.h
@@ -44,12 +44,12 @@
     The roguelike.h library can be included in your project in one of two different ways:
         - Copy and paste the roguelike.h file directly into your source tree.
         - Clone the GitHub as a git submodule to your project's repository.
-          - In bash console from the root directory of your project's repository:
+          1. In bash console from the root directory of your project's repository:
 
                 git submodule add https://github.com/Journeyman-dev/roguelike.h
                 git submodule update --init
 
-          - In your project's top level CMakeLists.txt:
+          2. In your project's top level CMakeLists.txt:
 
                 add_subdirectory(roguelike.h)
                 target_add_link_libraries(YOUR_TARGET_NAME PUBLIC rlh)
